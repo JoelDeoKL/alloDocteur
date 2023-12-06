db9523b9f94b74075947096af860e555e7499154
JoelDeoKL <joeldeocm88@gmail.com>
2023-12-06 13:02:56 +0100

    Mise en place de la logique de signal cote patient et admin

---
+++ src/Controller/SignalController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Signal;
+use App\Form\SignalType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SignalController extends AbstractController
+{
+    #[Route('/signals', name: 'signals')]
+    public function signals(EntityManagerInterface $entityManager): Response
+    {
+        $signals = $entityManager->getRepository(Signal::class)->findAll();
+
+        return $this->render('admin/signals.html.twig', ['signals' => $signals]);
+    }
+
+    #[Route('/mes_signals', name: 'mes_signals')]
+    public function mes_signals(EntityManagerInterface $entityManager): Response
+    {
+        $signals = $entityManager->getRepository(Signal::class)->findAll();
+
+        return $this->render('patient/signals.html.twig', ['signals' => $signals]);
+    }
+
+    #[Route('/editer_signal/{id?0}', name: 'editer_signal')]
+    public function editer_signal(Signal $signal = null, EntityManagerInterface $entityManager, ManagerRegistry $doctrine, Request $request, UserPasswordHasherInterface $userPasswordHasher): Response
+    {
+        $new = false;
+        if(!$signal){
+            $new = true;
+            $signal = new Signal();
+        }
+
+        $form = $this->createForm(SignalType::class, $signal);
+
+        //dd($request->request);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+
+            $dateD = new \DateTime();
+            $signal->setDateSignal($dateD);
+
+            $manager = $doctrine->getManager();
+            $manager->persist($signal);
+
+            $manager->flush();
+
+            if($new){
+                $message = "Le signal a été ajouter avec succès";
+            }else{
+                $message = "Le signal a été editer avec succès";
+            }
+
+            $this->addFlash("succes", $message);
+
+            return $this->redirectToRoute("signals");
+        }else{
+            return $this->render('patient/add_signal.html.twig', [
+                'form' => $form->createView()
+            ]);
+        }
+    }
+
+    #[Route('/detail_signal/{id<\d+>}', name: 'detail_signal')]
+    public function detail_signal(ManagerRegistry $doctrine, Signal $signal= null, $id): Response
+    {
+        if(!$signal){
+            $this->addFlash('error', "Ce signal n'existe pas !");
+            return $this->redirectToRoute("signals");
+        }
+        return $this->render('admin/signal_details.html.twig', ['signal' => $signal]);
+    }
+
+    #[Route('/signal_detail/{id<\d+>}', name: 'signal_detail')]
+    public function signal_detail(ManagerRegistry $doctrine, Signal $signal= null, $id): Response
+    {
+        if(!$signal){
+            $this->addFlash('error', "Ce signal n'existe pas !");
+            return $this->redirectToRoute("signals");
+        }
+        return $this->render('admin/signal_details.html.twig', ['signal' => $signal]);
+    }
+
+    #[Route('/delete_signal/{id?0}', name: 'delete_signal')]
+    public function delete_signal(Signal $signal = null, ManagerRegistry $doctrine, Request $request, $id): Response
+    {
+
+        $repository = $doctrine->getRepository(Signal::class);
+        $signal = $repository->find($id);
+
+        $manager = $doctrine->getManager();
+        $manager->remove($signal);
+
+        $manager->flush();
+
+        $message = "Le signal a été supprimer avec succès";
+
+
+        $this->addFlash("succes", $message);
+
+        return $this->redirectToRoute("signals");
+
+    }
+
+}
--- src/Form/SignalType.php
+++ src/Form/SignalType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Signal;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,6 +17,7 @@ class SignalType extends AbstractType
             ->add('date_signal')
             ->add('patient')
             ->add('personnel')
+            ->add('editer', SubmitType::class)
         ;
     }
 
