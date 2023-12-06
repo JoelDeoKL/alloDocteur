9695f3a7f6094a113b0c1354c01040ded05e3ebc
JoelDeoKL <joeldeocm88@gmail.com>
2023-12-06 12:54:09 +0100

    Mise en place de la logique ordonnance

---
+++ src/Controller/OrdonnanceController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ordonnance;
+use App\Form\OrdonnanceType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrdonnanceController extends AbstractController
+{
+    #[Route('/ordonnances', name: 'ordonnances')]
+    public function ordonnances(EntityManagerInterface $entityManager): Response
+    {
+        $ordonnances = $entityManager->getRepository(Ordonnance::class)->findAll();
+
+        return $this->render('admin/ordonnances.html.twig', ['ordonnances' => $ordonnances]);
+    }
+
+    #[Route('/editer_ordonnance/{id?0}', name: 'editer_ordonnance')]
+    public function editer_ordonnance(Ordonnance $ordonnance = null, EntityManagerInterface $entityManager, ManagerRegistry $doctrine, Request $request, UserPasswordHasherInterface $userPasswordHasher): Response
+    {
+        $new = false;
+        if(!$ordonnance){
+            $new = true;
+            $ordonnance = new Ordonnance();
+        }
+
+        $form = $this->createForm(OrdonnanceType::class, $ordonnance);
+
+        //dd($request->request);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+
+            $dateD = new \DateTime();
+            $ordonnance->setDateCreation($dateD);
+
+            $manager = $doctrine->getManager();
+            $manager->persist($ordonnance);
+
+            $manager->flush();
+
+            if($new){
+                $message = "Le ordonnance a été ajouter avec succès";
+            }else{
+                $message = "Le ordonnance a été editer avec succès";
+            }
+
+            $this->addFlash("succes", $message);
+
+            return $this->redirectToRoute("ordonnances");
+        }else{
+            return $this->render('admin/add_ordonnance.html.twig', [
+                'form' => $form->createView()
+            ]);
+        }
+    }
+
+    #[Route('/detail_ordonnance/{id<\d+>}', name: 'detail_ordonnance')]
+    public function detail_ordonnance(ManagerRegistry $doctrine, Ordonnance $ordonnance= null, $id): Response
+    {
+        if(!$ordonnance){
+            $this->addFlash('error', "Ce ordonnance n'existe pas !");
+            return $this->redirectToRoute("ordonnances");
+        }
+        return $this->render('admin/ordonnance_details.html.twig', ['ordonnance' => $ordonnance]);
+    }
+
+    #[Route('/delete_ordonnance/{id?0}', name: 'delete_ordonnance')]
+    public function delete_ordonnance(Ordonnance $ordonnance = null, ManagerRegistry $doctrine, Request $request, $id): Response
+    {
+
+        $repository = $doctrine->getRepository(Ordonnance::class);
+        $ordonnance = $repository->find($id);
+
+        $manager = $doctrine->getManager();
+        $manager->remove($ordonnance);
+
+        $manager->flush();
+
+        $message = "Le ordonnance a été supprimer avec succès";
+
+
+        $this->addFlash("succes", $message);
+
+        return $this->redirectToRoute("ordonnances");
+
+    }
+}
